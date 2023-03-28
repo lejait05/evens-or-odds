f85e255d7926b8ffa3f3c0cee74602cc21f3eab2
lineisha gibson <lineishagibson@gmail.com>
2023-03-27 22:42:02 -0500

    src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {configureStore} from "@reduxjs/toolkit";
 import App from './components/App';
 import  './index.css';
 
+const DEFAULT_SETTINGS = {
+    gameStarted: false,
+    instructionsExpanded: false
+};
+const rootReducer = ()=>{
+    return DEFAULT_SETTINGS;
+};
 
+const store = configureStore({reducer: rootReducer});
+
+console.log('store', store);
+console.log('store.getState()', store.getState() );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App/>);

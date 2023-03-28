8264c8946d2c068ed8a81974b78371a2d004d544
lineisha gibson <lineishagibson@gmail.com>
2023-03-27 23:14:18 -0500

    actions

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {configureStore} from "@reduxjs/toolkit";
 import App from './components/App';
-import  './index.css';
+import './index.css';
 
 const DEFAULT_SETTINGS = {
     gameStarted: false,
     instructionsExpanded: false
 };
-const rootReducer = ()=>{
-    return DEFAULT_SETTINGS;
+const rootReducer = (state= DEFAULT_SETTINGS, action) => {
+    console.log('state', state, 'action', action);
+    if (action.type === 'SET_GAME_STARTED') {
+        return {
+            gameStarted: action.gameStarted,
+            instructionsExpanded: false
+        }
+    }
+    return state;
 };
 
 const store = configureStore({reducer: rootReducer});
-
 console.log('store', store);
-console.log('store.getState()', store.getState() );
+store.subscribe(() => {
+    console.log('store.getState()', store.getState());
+});
+
+const action1 = {type: 'SET_GAME_STARTED', gameStarted: true};
+store.dispatch(action1);
+
+store.dispatch({type: 'foo'});
+store.dispatch({type: 'boo'});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <App/>);
+root.render(<App/>);

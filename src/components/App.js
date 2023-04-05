30bca0b5c3b18054e024662446a5a01d5662f957
lineisha gibson <lineishagibson@gmail.com>
2023-04-04 22:53:53 -0500

    record

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Instructions from "./Instructions";
 import DrawCard from "./DrawCard";
 import Card from "./Card";
 import Guess from "./Guess";
+import GameState from './GameState';
 
 
 class App extends Component {
@@ -32,6 +33,7 @@ class App extends Component {
                     this.props.gameStarted ? (
                         <div>
                             <h3> The game is on!</h3>
+                            <GameState/>
                             <br/>
                             <Guess/>
                             <br/>
